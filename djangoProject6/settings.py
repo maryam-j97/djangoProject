d0f5e4cb431b8801fb680c5bac70bd0fbca14038
maryam_j97 <maryamjali7@gmai.com>
2021-02-12 23:06:31 +0300

    few Html changes

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoProject6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'matchinfo/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- matchinfo/views.py
+++ matchinfo/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-
 # Create your views here.
 def Home(request):
     home = Hometeam.objects.all()
     away = Awayteam.objects.all()
-    context = {'home': home, 'away': away}
-    return render(request, "./matchtemp.html", context)
+    context={'home':home,'away':away}
+    return render(request, "./match.html",context)
